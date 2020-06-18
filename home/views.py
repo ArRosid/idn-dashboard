18850683ae24a6b046d5c19ed9ad89a53f8154a9
ArRosid <ahmadrosid30121997@gmail.com>
2020-06-18 08:52:24 +0700

    harus melengkapi profile sebelum hi idn

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
@@ -15,6 +16,13 @@ def home(request):
 
 
 def hi_idn(request):
+    if not request.user.profile.is_valid():
+        messages.error(
+            request,
+            "Anda harus melengkap profile Anda sebelum menghubungi admin, <a href='accounts/profile/'>disini</a>",
+        )
+        return redirect("home:home")
+
     if request.method == "POST":
         if request.user.is_authenticated:
             form = HiIDNFormsAuthenticated(request.POST)
