4965a686547f6ea9625f5ab07988440a38feafbf
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2020-07-09 16:47:08 +0700

    Add training terkait di interaksi models

--- marketing/forms.py
+++ marketing/forms.py
@@ -5,4 +5,13 @@ from marketing.models import Interaksi
 class InteraksiForm(forms.ModelForm):
     class Meta:
         model = Interaksi
-        fields = ("nama_client", "sumber", "no_hp", "email", "fb", "ig", "topik")
+        fields = (
+            "nama_client",
+            "training_terkait",
+            "sumber",
+            "no_hp",
+            "email",
+            "fb",
+            "ig",
+            "topik",
+        )
